3bcf5e704e0bf24da74af39a914b920d417cb216
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-04 12:41:44 +0300

    Scripted auto-commit on change (2021-12-04 12:41:44) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/JSTesterApp.java
+++ src/main/java/ru/bmstu/lab4/JSTesterApp.java
@@ -1,7 +1,9 @@
 package main.java.ru.bmstu.lab4;
 
+import akka.actor.ActorSystem;
+
 public class JSTesterApp {
     public static void main(String[] args) {
-        
+        ActorSystem actorSystem = 
     }
 }
