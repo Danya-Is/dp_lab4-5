0c6dc3e2c22032ef104c91f8faae5d9af65108b8
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-04 12:46:58 +0300

    Scripted auto-commit on change (2021-12-04 12:46:58) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/ExecuteActor.java
+++ src/main/java/ru/bmstu/lab4/ExecuteActor.java
@@ -1,10 +1,12 @@
 package main.java.ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ExecuteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match();
     }
 }
