da41fc115bbd735c2ca7facbe5c8f0f2a533c8c2
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-05 23:07:57 +0300

    Scripted auto-commit on change (2021-12-05 23:07:57) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/StorageActor.java
+++ src/main/java/ru/bmstu/lab4/StorageActor.java
@@ -3,14 +3,17 @@ package main.java.ru.bmstu.lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class StorageActor  extends AbstractActor {
-    private Map<Integer, String> store;
+    private final Map<Integer, String> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(Result.class, );
+                .match(Result.class, msg -> );
     }
+
+    private void addMessage
 }
