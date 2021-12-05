b16475a75fdf3c8cbb9de0f92331ea4254a1511a
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-06 00:32:53 +0300

    Scripted auto-commit on change (2021-12-06 00:32:53) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/JSTesterApp.java
+++ src/main/java/ru/bmstu/lab4/JSTesterApp.java
@@ -4,6 +4,7 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 
 public class JSTesterApp {
@@ -13,4 +14,6 @@ public class JSTesterApp {
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
 
     }
+
+    private Route createRoute()
 }
