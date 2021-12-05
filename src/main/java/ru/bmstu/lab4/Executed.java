67565d870e8e80c43ae46792a7370398174656bc
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-05 22:36:01 +0300

    Scripted auto-commit on change (2021-12-05 22:36:01) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Executed.java
+++ src/main/java/ru/bmstu/lab4/Executed.java
@@ -1,4 +1,25 @@
 package main.java.ru.bmstu.lab4;
 
 public class Executed {
+    private final String jsScript;
+    private final String functionName;
+    private final int[] params;
+
+    public Executed(String jsScript, String functionName, int[] params) {
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.params = params;
+    }
+
+    public int[] getParams() {
+        return params;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
 }
