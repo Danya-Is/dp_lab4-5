ba701f336940e076c6d1039faba58cf65c80a4a2
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-05 23:35:47 +0300

    Scripted auto-commit on change (2021-12-05 23:35:47) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/lab4/RouterActor.java
@@ -1,9 +1,18 @@
 package main.java.ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
 public class RouterActor extends AbstractActor {
+
+    private final ActorRef storage;
+
+    public RouterActor() {
+        storage = getContext().actorOf(new Props(StorageActor.class));
+    }
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
