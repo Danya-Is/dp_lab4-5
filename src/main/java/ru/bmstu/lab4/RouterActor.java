0c4cddaf8dd5c4ccdd5d695f3e1ee37d537bbecc
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-05 23:37:55 +0300

    Scripted auto-commit on change (2021-12-05 23:37:54) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/lab4/RouterActor.java
@@ -10,13 +10,15 @@ public class RouterActor extends AbstractActor {
     private final ActorRef storage;
 
     public RouterActor() {
-        storage = getContext().actorOf(new Props(StorageActor.class));
+        storage = getContext().actorOf(Props.create(StorageActor.class));
     }
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(PostRequest.class, );
+                .match(PostRequest.class, this::executeTests)
+                .match(GetRequest.class, msg -> storage.tell(msg, ))
+                .build();
     }
 
     private void executeTests(PostRequest msg) {
