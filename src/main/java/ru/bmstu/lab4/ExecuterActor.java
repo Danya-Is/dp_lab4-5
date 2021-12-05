10583c2bd6cae6d2d99b52938af3f3fe2ded2aa8
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-05 22:21:16 +0300

    Scripted auto-commit on change (2021-12-05 22:21:16) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/ExecuterActor.java
+++ src/main/java/ru/bmstu/lab4/ExecuterActor.java
@@ -3,6 +3,7 @@ package main.java.ru.bmstu.lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -17,5 +18,6 @@ public class ExecuterActor extends AbstractActor {
     public void execute(PostRequest msg) throws ScriptException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval(msg.getJsScript());
+        Invocable invocable = (Invocable) engine;
     }
 }
