9cd997db9403ef15db75ceadb365d53bea3a31ab
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-06 20:02:36 +0300

    Scripted auto-commit on change (2021-12-06 20:02:36) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Messages/GetRequest.java
+++ src/main/java/ru/bmstu/lab4/Messages/GetRequest.java
@@ -1,13 +1,13 @@
 package main.java.ru.bmstu.lab4.Messages;
 
 public class GetRequest {
-    private final int packageID;
+    private final int packageId;
 
-    public GetRequest(int packageID) {
-        this.packageID = packageID;
+    public GetRequest(int packageId) {
+        this.packageId = packageId;
     }
 
     public int getPackageID() {
-        return packageID;
+        return packageId;
     }
 }
