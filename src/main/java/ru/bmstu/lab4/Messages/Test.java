dd2408c863c1644ea54fc7b8adbf9438e92c0c69
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-06 19:10:06 +0300

    Scripted auto-commit on change (2021-12-06 19:10:06) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Messages/Test.java
+++ src/main/java/ru/bmstu/lab4/Messages/Test.java
@@ -2,25 +2,27 @@ package main.java.ru.bmstu.lab4.Messages;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class Test {
     @JsonProperty("testName")
     private final String testName;
     @JsonProperty("expectedResult")
-    private final float expectedResult;
+    private final String expectedResult;
     @JsonProperty("params")
-    private final int[] params;
+    private final ArrayList<Integer> params;
 
-    public Test(String testName, float expectedResult, int[] params) {
+    public Test(String testName, String expectedResult, ArrayList<Integer> params) {
         this.testName = testName;
         this.expectedResult = expectedResult;
         this.params = params;
     }
 
-    public int[] getParams() {
+    public ArrayList<Integer> getParams() {
         return params;
     }
 
-    public float getExpectedResult() {
+    public String getExpectedResult() {
         return expectedResult;
     }
 }
