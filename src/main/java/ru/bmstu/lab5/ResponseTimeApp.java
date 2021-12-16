334d1b40a841e7b1e3e1b16801386e8477356baf
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-16 22:03:14 +0300

    Scripted auto-commit on change (2021-12-16 22:03:14) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/lab5/ResponseTimeApp.java
@@ -14,6 +14,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
 import akka.util.Timeout;
+import org.asynchttpclient.AsyncHttpClient;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -52,7 +53,8 @@ public class ResponseTimeApp {
     private static Sink createSink() {
         Flow.<Pair<String, Integer>>create()
                 .mapConcat(pair -> Collections.nCopies(pair.second(), pair.first()))
-                .mapAsync(4, pair -> {
+                .mapAsync(4, url -> {
+                    AsyncHttpClient client = 
                 })
     }
 }
