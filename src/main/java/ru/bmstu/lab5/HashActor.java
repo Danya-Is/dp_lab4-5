041c07108f1e9cf7e19352128910cc500e2c5c44
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-16 20:47:26 +0300

    Scripted auto-commit on change (2021-12-16 20:47:26) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/HashActor.java
+++ src/main/java/ru/bmstu/lab5/HashActor.java
@@ -2,7 +2,12 @@ package ru.bmstu.lab5;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+
 public class HashActor extends AbstractActor {
+
+    private HashMap<String, Float> 
+
     @Override
     public Receive createReceive() {
         return null;
