0a3cd0d36b110a9e5f291236da12fde9a58991a0
Ismagilova Daria <armi.din1902@gmail.com>
2021-12-16 20:06:03 +0300

    Scripted auto-commit on change (2021-12-16 20:06:03) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/lab5/ResponseTimeApp.java
@@ -5,26 +5,31 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 public class ResponseTimeApp {
     public static final String STARTED = "Started";
     public static final String TEST_URL = "testUrl";
+    public static final String LOCALHOST = "localhost";
+    public static final String COUNT = "count";
 
     public static void main(String[] args) {
         System.out.println(STARTED);
         ActorSystem actorSystem = ActorSystem.create("routes");
         final Http http = Http.get(actorSystem);
         final ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(actorSystem, actorMaterializer);
 
     }
 
     private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorSystem actorSystem, ActorMaterializer actorMaterializer) {
         return Flow.of(HttpRequest.class)
                 .map(request -> {
-                    request.getUri().query().get(TEST_URL);
+                    Query query = request.getUri().query();
+                    String url = query.get(TEST_URL).orElse(LOCALHOST);
+                    int count = Integer.parseInt(query.get(COUNT).orElse("1"));
                 })
     }
 }
